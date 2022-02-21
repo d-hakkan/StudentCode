b294a5fb66e8be7581d2e921d612c36e6988150f
Hakkan Nabila Darmawan <71591994+d-hakkan@users.noreply.github.com>
2022-02-21 09:35:31 +0700

    Add files via upload

---
+++ Javascript/latihan3/script.js
@@ -0,0 +1,11 @@
+alert("Welcome");
+var lagi = true;
+
+while(lagi === true){
+    var nama = prompt("masukkan nama: ");
+    alert("Halo "+nama);
+
+    lagi = confirm("coba lagi?");
+}
+
+alert("Thanks");
