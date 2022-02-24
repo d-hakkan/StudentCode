2075afa4889ce7f8751ed62892714b8fd70a4140
Hakkan Nabila Darmawan <71591994+d-hakkan@users.noreply.github.com>
2022-02-24 09:48:20 +0700

    Add files via upload

---
+++ Javascript/latihan7/script.js
@@ -0,0 +1,9 @@
+var angka = prompt("Masukkan angka ");
+
+if(angka % 2 == 0){
+    alert(angka + " adalah genap");
+}else if(angka % 2 == 1){
+    alert(angka + " adalah ganjil");
+}else{
+    alert("bukan angka");
+}
