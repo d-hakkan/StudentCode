d653db4df1fc14508c9ecd4d796328e6e3fab0ad
Hakkan Nabila Darmawan <71591994+d-hakkan@users.noreply.github.com>
2022-02-25 22:29:46 +0700

    Add files via upload

---
+++ Javascript/latihan17/latihan18/script.js
@@ -0,0 +1,10 @@
+var arr = ["Muhammad", "James", "Alfatih"];
+//splice
+/* arr.splice(1,2, "James");
+console.log(arr.join(" - ")); */
+
+//slice
+var arr = ["Muhammad", "James", "Alfatih", "Amir"];
+var arr2 = arr.slice(1,3);
+console.log(arr.join(" - "));
+console.log(arr2.join(" - "));
