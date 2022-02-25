94b34a7643d71cddf782abd1593fd741dfada79b
Hakkan Nabila Darmawan <71591994+d-hakkan@users.noreply.github.com>
2022-02-25 22:15:45 +0700

    Add files via upload

---
+++ Javascript/latihan17/script.js
@@ -0,0 +1,19 @@
+/*var arr = ["Hakkan", "Nabila", "Darmawan", "James"];
+
+for(var i = 0; i < arr.length; i++){
+    console.log ("Mahasiswa ke-"+(i+1)+" "+arr[i]);
+}*/
+
+//join
+var arr = ['Hakkan', 'Nabila', 'Darmawan'];
+//console.log(arr.join(' - '));
+
+//push dan pop
+//arr.push('James');
+//arr.pop();
+//console.log(arr.join(' - '));
+
+//3. unshift dan shift
+//arr.unshift('James');
+arr.shift();
+console.log(arr.join(' - '));
