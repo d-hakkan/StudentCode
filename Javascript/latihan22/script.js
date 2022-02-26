2ae8a5f9be18008a000349d8d56022db49d32738
Hakkan Nabila Darmawan <71591994+d-hakkan@users.noreply.github.com>
2022-02-26 08:12:09 +0700

    Add files via upload

---
+++ Javascript/latihan22/script.js
@@ -0,0 +1,10 @@
+var mhs = {
+    nama : "Hakan Alfatih",
+    umur : 20,
+    ips : [4.0, 4.0, 3.9],
+    alamat : {
+        jalan : "Jl. Tokyo 123",
+        kota : "Malang",
+        prov : "California"
+    }
+};
